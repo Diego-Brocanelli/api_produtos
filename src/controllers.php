188344d3b31f017207aef70d1b4d8a54551c7220
Diego Brocanelli <diegod2@msn.com>
2015-05-30 09:57:58 -0300

    Organização de controllers em arquivos.

--- src/controllers.php
+++ src/controllers.php
@@ -6,26 +6,15 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-//documentação 
+//index
 $app->get('/', function () use ($app) {
     return $app['twig']->render('doc.html', array());
-})
-->bind('homepage');
-//lista de produtos
-$app->get('/api/v1/produtos', function () use ($app) {
-    $sql = 'SELECT * FROM produtos;';
-    $produtos = $app['db']->fetchAssoc($sql);
-    //em caso de nenhum produto cadastrado
-    if($produtos === false)
-        $produtos = json_encode(['mensagem:'=>' Nenhum produto cadastrado']);
-
-    return $app->json($produtos);
-});
-//documentação
-$app->get('/api/v1/docs', function () use ($app) {
-    return $app['twig']->render('doc.html', array());
-});
-
+})->bind('homepage');
+//products controller
+$app->mount('/api/v1', include 'Api/Controller/ProductsController.php');
+//docs controller
+$app->mount('/api/v1', include 'Api/Controller/DocController.php');
+//excpetion controller
 $app->error(function (\Exception $e, $code) use ($app) {
     if ($app['debug']) {
         return;
